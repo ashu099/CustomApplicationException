058b5b0680d2e5c287dd8e7dff6fc2599d7a09d4
Ashutosh <ashutosh.chaturvedi@euromonitor.com>
2024-07-11 22:05:44 +0530

    test

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CsutomApplicationException
+{
+
+    public class DivideByOddNoException : ApplicationException
+    {
+        public override string Message
+        {
+            get {
+                return "Attempted to divide by odd number";
+            }
+        }
+    }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                int x = int.Parse(Console.ReadLine());
+                int y = int.Parse(Console.ReadLine());
+                int z = x / y;
+                if (y % 2 > 0)
+                {
+
+                       throw new DivideByOddNoException();
+                }
+                Console.WriteLine("result:" + z);
+          
+           
+          
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
